eca26ab7932d2021362af1e6d0f7788090b3ce89
labita.leonard@gmail.com <labita.leonard@gmail.com>
2015-10-15 21:46:52 -0400

    Revealing Module Pattern for Pomodoro

--- app/scripts/freeCodeCamp/pomodoro.js
+++ app/scripts/freeCodeCamp/pomodoro.js
@@ -1,138 +1,162 @@
-$(document).ready(function() {
-  var session_len = 25;
-  var break_len = 5;
-  var clock_min = session_len;
-  var clock_sec = 0;
-  var isStop = true;
-  var interval;
-  
+var Pomodoro = (function(){
 
-  function updateTime() {
-    var min = clock_min;
-    var sec = clock_sec;
+	// Settings
+	var session_len = 25;
+  	var break_len = 5;
+  	var clock_min = session_len;
+  	var clock_sec = 0;
+  	var isStop = true;
+  	var interval;
+
+  	// Cache DOM
+  	var $al = $(".inner");
+	var $clock_time = $al.find(".clock-time");
+	var $clock_text = $al.find(".clock-text");
+	var $middle = $al.find(".middle");
+	var $el = $(".button-container");
+	var $ctrl = $el.find(".ctrl");
+	var $break_time = $el.find(".break-time");
+	var $break_plus = $el.find(".break-plus");
+	var $break_min = $el.find(".break-min");
+	var $session_time = $el.find(".session-time");
+	var $session_plus = $el.find(".session-plus");
+	var $session_min = $el.find(".session-min");
+
+	// Bind Events
+	$ctrl.on("click", startStop);
+	$break_min.on("click", breakMin);
+	$break_plus.on("click", breakPlus);
+	$session_min.on("click", sessionMin);
+	$session_plus.on("click", sessionPlus);
+
+	function startClock(){
+		if(clock_min !== 0 && clock_sec === 0) {
+	      	clock_sec = 59;
+	      	clock_min = clock_min - 1;
+	      	updateTime();
+	    }
+	    else if (clock_min === 0 && clock_sec === 0) {
+	      	if($(".clock-text").text() === "Session") {
+	        	$(".clock-text").text("Break");
+	        	clock_min = break_len;
+	        	updateTime();
+	        	playAlarm();
+	      	}
+	      	else {
+	        	isStop = true;
+	        	updateCtrlButton();
+	        	$(".clock-text").text("Session");
+	        	clock_min = session_len;
+	        	updateTime();
+	        	clearInterval(interval);
+	        	pauseTicking();
+	      	}
+	    }
+	   	else {
+	      	clock_sec = clock_sec - 1;
+	      	updateTime();
+	    }
+	}
+
+	function startStop(){
+		isStop = !isStop;
+	    updateCtrlButton();
+	    if(!isStop) {
+	      	interval = setInterval(startClock, 1000);
+	      	playTicking();
+	    }
+	    else {
+	      	isStop = true;
+	      	updateCtrlButton();
+	     	$(".clock-text").text("Session");
+	      	clock_min = session_len;
+	      	clock_sec = 0;
+	      	updateTime();
+	      	$(".middle").css("background" , "linear-gradient(to bottom, rgba(255,255,255,1) 100%, rgba(254,51,50,1) 0%, rgba(254,51,50,1) 0%)");
+	      	clearInterval(interval);
+	      	pauseTicking();
+	   	}
+	}
+
+	function updateTime(){
+		var min = clock_min;
+    	var sec = clock_sec;
     
-    if(min < 10){
-      min = "0" + min;
-    }
-    if(sec < 10){
-      sec = "0" + sec;
-    }
-    $(".clock-time").text(min + ":" + sec);
-  }
-  
-  // Decrease break length
+    	if(min < 10){
+      		min = "0" + min;
+    	}
+    	if(sec < 10){
+      		sec = "0" + sec;
+    	}
+    	$(".clock-time").text(min + ":" + sec);
+	}
 
-  $(".break-min").click(function() {
-    if(isStop && break_len > 1) {
-      break_len = break_len - 1;
-      $(".break-time").text(break_len);
-    }
-  });
+	function breakMin(){
+		if(isStop && break_len > 1) {
+	      	break_len = break_len - 1;
+	      	$(".break-time").text(break_len);
+	    }
+	}
 
-  // Increase break length
+	function breakPlus(){
+		if(isStop) {
+	      	break_len = break_len + 1;
+	      	$(".break-time").text(break_len);
+	    }
+	}
 
-  $(".break-plus").click(function() {
-    if(isStop) {
-      break_len = break_len + 1;
-      $(".break-time").text(break_len);
-    }
-  });
-  
-  // Decrease session length
+	function sessionMin(){
+		if(isStop && session_len > 1) {
+	      	session_len = session_len - 1;
+	      	clock_min = session_len;
+	      	updateTime();
+	      	$(".session-time").text(session_len);
+	    }
+	}
 
-  $(".session-min").click(function() {
-    if(isStop && session_len > 1) {
-      session_len = session_len - 1;
-      clock_min = session_len;
-      updateTime();
-      $(".session-time").text(session_len);
-    }
-  });
+	function sessionPlus(){
+		if(isStop) {
+	      	session_len = session_len + 1;
+	      	clock_min = session_len;
+	      	updateTime();
+	      	$(".session-time").text(session_len);
+	    }
+	}
 
-  // Increase session length
+	function updateCtrlButton(){
+		if(isStop) {
+	      	$(".ctrl").removeClass("btn-danger");
+	      	$(".ctrl").addClass("btn-success");
+	      	$(".ctrl").text("Start");
+	    }
+	    else {
+	      	$(".ctrl").removeClass("btn-success");
+	      	$(".ctrl").addClass("btn-danger");
+	      	$(".ctrl").text("Stop");
+	    }
+	}
 
-  $(".session-plus").click(function() {
-    if(isStop) {
-      session_len = session_len + 1;
-      clock_min = session_len;
-      updateTime();
-      $(".session-time").text(session_len);
-    }
-  });
-  
-  // Update ctrl button
+	function fillCircle(){
+		var total_sec = ($(".clock-text").text() === "Session") ? session_len * 60 : break_len * 60;
+	    var current_sec = (clock_min) * 60 + clock_sec;
+	    var percentage = (current_sec/total_sec) * 100;
+	    $(".middle").css("background" , "linear-gradient(to bottom, rgba(255,255,255,1) "+percentage+"%, rgba(254,51,50,1) 0%, rgba(254,51,50,1) 0%)");
+	}
 
-  function updateCtrlButton() {
-    if(isStop) {
-      $(".ctrl").removeClass("btn-danger");
-      $(".ctrl").addClass("btn-success");
-      $(".ctrl").text("Start");
-    }
-    else {
-      $(".ctrl").removeClass("btn-success");
-      $(".ctrl").addClass("btn-danger");
-      $(".ctrl").text("Stop");
-    }
-  }
-  
-  //Fill Circle
+	function playTicking(){
+		var ticking = document.getElementById("ticking");
+	    ticking.play();
+	}
 
-  function fillCircle() {
-    var total_sec = ($(".clock-text").text() === "Session") ? session_len * 60 : break_len * 60;
-    var current_sec = (clock_min) * 60 + clock_sec;
-    var percentage = (current_sec/total_sec) * 100;
-    $(".middle").css("background" , "linear-gradient(to bottom, rgba(255,255,255,1) "+percentage+"%, rgba(254,51,50,1) 0%, rgba(254,51,50,1) 0%)");
-  }
-  fillCircle();
+	function pauseTicking(){
+		var ticking = document.getElementById("ticking");
+	    ticking.pause();
+	    ticking.currentTime = 0;
+	}
 
-  function startClock() {
-    
-    fillCircle();
-    if(clock_min !== 0 && clock_sec === 0) {
-      clock_sec = 59;
-      clock_min = clock_min - 1;
-      updateTime();
-    }
-    else if (clock_min === 0 && clock_sec === 0) {
-      if($(".clock-text").text() === "Session") {
-        $(".clock-text").text("Break");
-        clock_min = break_len;
-        updateTime();
-      }
-      else {
-        isStop = true;
-        updateCtrlButton();
-        $(".clock-text").text("Session");
-        clock_min = session_len;
-        updateTime();
-        clearInterval(interval);
-      }
-    }
-    else {
-      clock_sec = clock_sec - 1;
-      updateTime();
-    }
-  }
-  
-  // Start or stop clock
-  
-  $(".ctrl").click(function() {
-    isStop = !isStop;
-    updateCtrlButton();
-    if(!isStop) {
-      interval = setInterval(startClock, 1000);
-    }
-    else {
-      isStop = true;
-      updateCtrlButton();
-      $(".clock-text").text("Session");
-      clock_min = session_len;
-      clock_sec = 0;
-      updateTime();
-      $(".middle").css("background" , "linear-gradient(to bottom, rgba(255,255,255,1) 100%, rgba(254,51,50,1) 0%, rgba(254,51,50,1) 0%)");
-      clearInterval(interval);
-    }
-  });
+	function playAlarm(){
+		var alarm = document.getElementById("alarm");
+	    alarm.play();
+	}
 
-});
-
+})()
---
+++ app/scripts/freeCodeCamp/twitch.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+	
+	var streams = ["FreeCodeCamp", "DevWars", "MedryBW", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"]
+
+	var createStreamerList = function(streams){
+		var streamerItem;
+		var streamerList = '';
+		for (var i = 0; i < streams.length; i++) {
+        streamerItem = '<li id="' + streams[i] + '" class="streamer-block"><a href="http://twitch.tv/' + streams[i] + '"><ul class="streamer-info clearfix"><li id="' + streams[i] + '-image" class="user-image"><img width="50px" height="50px" src="http://placehold.it/50x50" /></li><li id="' + streams[i] + '-name" class="user-name"></li><li id="' + streams[i] + '-status" class="user-status"><span class="glyphicon glyphicon-remove"></span></li><li id="' + streams[i] + '-description" class="user-description"></li></ul></a></li>';
+        streamerList += streamerItem;
+        }
+      $('#streamerList').append(streamerList);
+	};
+
+	var injectStreamerData = function(streams){
+		var streamerId, channelsURL, streamsURL;
+		for (var i=0; i < streams.length; i++) {
+      		(function(i) {
+        		streamerId = streams[i];
+        		channelsURL = 'https://api.twitch.tv/kraken/channels/' + streamerId + '?callback=?';
+        		streamsURL = 'https://api.twitch.tv/kraken/streams/' + streamerId + '?callback=?';
+        
+        	$.getJSON(channelsURL, function(channelResults) {        
+          		$('#' + streams[i] + '-name').html(channelResults.display_name);
+          	if (channelResults.logo !== null) {
+          		$('#' + streams[i] + '-image img').attr("src", channelResults.logo);
+          	}
+
+          	if (channelResults.status !== null) {
+            	if (channelResults.status.length > 36) {
+            	var clippedStatus = channelResults.status.slice(0,34) + '...';
+            	$('#' + streams[i] + '-description').html(clippedStatus).hide();
+            }
+          } 
+        });
+
+        $.getJSON(streamsURL, function(streamResults) {
+          if (streamResults.stream !== null) {
+            $('#' + streams[i] + '-status span').removeClass('glyphicon glyphicon-remove').addClass('glyphicon glyphicon-ok');
+            $('#' + streams[i] + '-description').show();
+          }
+        });
+
+      	})(i);
+	  }
+	};
+
+	var tabSwitcher = function(){
+
+		$('#tabAll').click(function(){
+			$('#tabOnline, #tabOffline').removeClass('arrow');
+			$(this).addClass('arrow');
+			$('.streamer-block').show();
+		});
+
+		$('#tabOnline').click(function(){
+			$('#tabAll, #tabOffline').removeClass('arrow');
+			$(this).addClass('arrow');
+			$('streamer-block').show();
+			$('.glyphicon-remove').parents(".streamer-block").hide();
+
+		});
+
+		$('#tabOffline').click(function(){
+			$('#tabOnline, #tabAll').removeClass('arrow');
+			$(this).addClass('arrow');
+			$('.streamer-block').show();
+			$('.glyphicon-ok').parents(".streamer-block").hide();
+		});
+
+	};
+
+	var searchBar = function() {
+    	$('#search').keyup(function(){
+       		var valThis = $(this).val().toLowerCase();
+        	if(valThis == ""){
+            	$('#streamerList > li').show();           
+        	} 
+        	else {
+            	$('#streamerList > li').each(function(){
+                	var text = $(this).attr('id').toLowerCase();
+                	(text.indexOf(valThis) >= 0) ? $(this).show() : $(this).hide();
+        		});
+       		};
+    	});
+ 	};
+
+	createStreamerList(streams);
+	injectStreamerData(streams);
+	tabSwitcher();
+	searchBar();
+	
+});
